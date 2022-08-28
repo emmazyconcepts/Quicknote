93954d7c6534c8049535b00be40913321457753e
ekeemmanuel <emmazy100@gmail.com>
2022-08-28 20:31:24 +0100

    added some functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let myNote = [];
 var input = document.getElementById("input-el").value;
 let submitButton = document.getElementById("input-btn");
+let noteList = document.getElementsByid("note-list");
 
 submitButton.addEventListener("click", function () {
   var input = document.getElementById("input-el").value;
