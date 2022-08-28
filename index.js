3523d72b1383d0b111e2acbc1612d0f6e3190907
ekeemmanuel <emmazy100@gmail.com>
2022-08-28 20:36:39 +0100

    added some fuctions

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-let myNote = [];
+let myNote = ["asss", "assss", "ssss"];
 var input = document.getElementById("input-el").value;
 let submitButton = document.getElementById("input-btn");
-const noteList = document.getElementsByid("note-list");
+const noteList = document.getElementById("note-list");
 
 submitButton.addEventListener("click", function () {
   var input = document.getElementById("input-el").value;
   myNote.push(input);
   alert(myNote);
 });
+
+for (let i = 0; i < myNote.length; i++) {
+  console.log(myNote[i]);
+}
