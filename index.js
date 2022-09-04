b95487b83bf206d1e70f41920effa1a838ca2e53
ekeemmanuel <emmazy100@gmail.com>
2022-09-04 15:56:55 +0100

    restructured code

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ let myNote = [];
 const input = document.getElementById("input-el");
 const submitButton = document.getElementById("input-btn");
 const noteList = document.getElementById("note-list");
+const deleteBtn = document.getElementById("delete-btn");
 
 let noteLocalstorage = JSON.parse(localStorage.getItem("myNote"));
 if (noteLocalstorage) {
   myNote = noteLocalstorage;
-  saveNote();
+  render(myNote);
 }
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myNote = [];
+  render(myNote);
+});
 submitButton.addEventListener("click", function () {
   myNote.push(input.value);
   input.value = "";
   localStorage.setItem("myNote", JSON.stringify(myNote));
-  saveNote();
+  render(myNote);
   console.log(localStorage.getItem("myNote"));
 });
 
-function saveNote() {
+function render(Notes) {
   let listItems = "";
-  for (let i = 0; i < myNote.length; i++) {
+  for (let i = 0; i < Notes.length; i++) {
     listItems += `<li>
-    <a target="_blank" href="${myNote[i]}">
-    ${myNote[i]}
+    <a target="_blank" href="${Notes[i]}">
+    ${Notes[i]}
     </a>
     </li>`;
   }
