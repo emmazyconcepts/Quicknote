b20f4fc7e598b9cffbe1829a88a3002ee1035f9b
ekeemmanuel <emmazy100@gmail.com>
2022-09-06 03:24:34 +0100

    savetabs

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const input = document.getElementById("input-el");
 const submitButton = document.getElementById("input-btn");
 const noteList = document.getElementById("note-list");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 let noteLocalstorage = JSON.parse(localStorage.getItem("myNote"));
 if (noteLocalstorage) {
   myNote = noteLocalstorage;
   render(myNote);
 }
+
+const tabs = [{ url: "https://www.linkedin.com/in/per-harald-borgen/" }];
+
+tabBtn.addEventListener("click", function () {
+  console.log(tabs);
+});
+
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myNote = [];
